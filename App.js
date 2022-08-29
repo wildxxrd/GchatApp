19c70f3ae325c059fc16393c75a0c9eb0758cda4
Anthony Tran <atran5@ggc.edu>
2022-08-29 13:59:32 -0400

    Added firebase
    firebase json

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App() {
   return (

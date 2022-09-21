94ba8df91ff2ec12980181d3f8dfb481b8a1f8d8
Wilmer <wilmer_will@outlook.com>
2022-09-20 22:52:50 -0400

    Added Firebase Support plus account management

---
+++ Screens/MainScreen/MainScreen.js
@@ -0,0 +1,12 @@
+import { Text, View,} from "react-native";
+
+const MainScreen = () => {
+  
+    return (
+      <View>
+        <Text>Main Screen</Text>
+      </View>
+    );
+};
+
+export default MainScreen;
---
+++ config.js
@@ -0,0 +1,23 @@
+//firebase config key setup
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+
+
+//web app Firebase configuration
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCG6RLXWZJcXBkhL1qE5yEHkoDR15qoUCQ",
+  authDomain: "ggchat-f307f.firebaseapp.com",
+  projectId: "ggchat-f307f",
+  storageBucket: "ggchat-f307f.appspot.com",
+  messagingSenderId: "1091659122353",
+  appId: "1:1091659122353:web:2c5de2b3f992e496a04490"
+};
+
+
+  firebase.initializeApp(firebaseConfig);
+
+
+export { firebase };

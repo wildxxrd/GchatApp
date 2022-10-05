3ae174d21949cff35840d112194653447b9dc1fa
Joseph Ongchangco <43042517+kiyros@users.noreply.github.com>
2022-10-05 14:42:00 -0400

    Settings Dropdown (WIP)

--- Screens/Icons/SettingsButton.js
+++ Screens/Icons/SettingsButton.js
@@ -1,10 +1,24 @@
-import { StyleSheet, View, Button } from "react-native";
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import {View,Text,Picker, DropDownPicker} from 'react-native';
+import SelectList from 'react-native-dropdown-select-list';
+import React from 'react';
 
-const SettingsButton = (text) => {
+const SettingsButton = () => {
+
+  const [selected, setSelected] = React.useState("");
+
+  const data = [
+    {key : "1", value:'profile'},
+    {key : "2", value:'Logout'}
+  ]
+
+
+
   return (
     <View>
-      <SimpleLineIcons name="settings" size={50}/>
+      <SimpleLineIcons name="settings" function size={50}/>
+      <SelectList data = {data} setSelected={setSelected} />
+
     </View>
   );
 };

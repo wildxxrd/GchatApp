2ce83649adddfc5d6c6d2bf455632ebda14f84aa
kiyros <jongchangco@ggc.edu>
2022-11-13 19:22:23 -0500

    firebase fix

--- config.js
+++ config.js
@@ -3,8 +3,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-const db = firebase.firestore();
-
 //web app Firebase configuration
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,20 +11,19 @@ const firebaseConfig = {
   projectId: "ggchat-f307f",
   storageBucket: "ggchat-f307f.appspot.com",
   messagingSenderId: "1091659122353",
-  appId: "1:1091659122353:web:2c5de2b3f992e496a04490"
+  appId: "1:1091659122353:web:2c5de2b3f992e496a04490",
 };
 
+firebase.initializeApp(firebaseConfig);
 
-  firebase.initializeApp(firebaseConfig);
+// firebase db
+const db = firebase.firestore();
 
 // firebase methods
-export const getPainatedTasks = () =>{
-  const queryRef = db.collection('posts')
-    .limit(15);
-
+export const getPainatedTasks = () => {
+  const queryRef = db.collection("posts").limit(15);
 
   return queryRef.get();
-}
-
+};
 
 export { firebase };
